606f9d6444c9bc133fdfe9560d802d30750a4c6c
toru-hihumi <robo_hihumi@outlook.jp>
2017-12-28 08:56:50 +0900

    using namespace std;のような名前空間省略記法は、衝突の可能性があるので極力使わない方が良いらしい。 マイクロマウスの開発だと余計にその影響を受けそうなので今のうちから修正。

--- src/ex2.cpp
+++ src/ex2.cpp
@@ -8,20 +8,19 @@
 #include "stdint.h"
 #include <iostream>
 #include <String>
-using namespace std;
 
 int main(){
-	string name;
-	string answer_s;
+	std::string name;
+	std::string answer_s;
 	uint8_t answer_ui8;
-	cout << "あなたのお名前をお伺いしてもよろしいですか？y:1,n:0" << endl;
-	cin >> answer_s;
+	std::cout << "あなたのお名前をお伺いしてもよろしいですか？y:1,n:0" << std::endl;
+	std::cin >> answer_s;
 	answer_ui8 = std::stoi(answer_s);
 	if(answer_ui8 == 1){
-		cin >> name;
-		cout << "あなたは" << name << "さんと仰るのですね。こんにちは" << endl;
+		std::cin >> name;
+		std::cout << "あなたは" << name << "さんと仰るのですね。こんにちは" << std::endl;
 	} else {
-		cout << "このご時世、個人情報は大事ですからね。それでは。" << endl;
+		std::cout << "このご時世、個人情報は大事ですからね。それでは。" << std::endl;
 	}
 
 	return 0;
