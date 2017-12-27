837605cfa4a15bf8505c5e15a0db93bcf4c60ae1
toru-hihumi <robo_hihumi@outlook.jp>
2017-12-28 08:34:24 +0900

    commit

--- src/ex2.cpp
+++ src/ex2.cpp
@@ -1,16 +1,17 @@
-/*
- * ex2.cpp
- *
- *  Created on: 2017/12/28
- *      Author: PCーUSER
- */
-#include <iostream>
-using namespace std;
-
-int main(){
-
-	cout << "私は" << "2018" <<"年"<<"1"<<"月"<<"20"<<"日で" << "22" << "歳になる" <<endl;
-	cout << "私は2018年1月20日で22歳になる" << endl;
-	cout << "私は2018年1月20日で"<<21+1<<"歳になる"<<endl;
-	return 0;
-}
+///*
+// * ex2.cpp
+// *
+// *  Created on: 2017/12/28
+// *      Author: PCーUSER
+// */
+//#include <iostream>
+//using namespace std;
+//
+//int main(){
+//
+//	cout << "私は" << "2018" <<"年"<<"1"<<"月"<<"20"<<"日で" << "22" << "歳になる" <<endl;
+//	cout << "私は2018年1月20日で22歳になる" << endl;
+//	cout << "私は2018年1月20日で"<<21+1<<"歳になる"<<endl;
+//	return 0;
+//
+//}
