63dcf035ea25563655edce38e403e5cee6653f0e
toru-hihumi <robo_hihumi@outlook.jp>
2017-12-28 08:41:04 +0900

    基本対話とか。 Stringの理解

--- src/ex2.cpp
+++ src/ex2.cpp
@@ -1,17 +1,28 @@
-///*
-// * ex2.cpp
-// *
-// *  Created on: 2017/12/28
-// *      Author: PCーUSER
-// */
-//#include <iostream>
-//using namespace std;
-//
-//int main(){
-//
-//	cout << "私は" << "2018" <<"年"<<"1"<<"月"<<"20"<<"日で" << "22" << "歳になる" <<endl;
-//	cout << "私は2018年1月20日で22歳になる" << endl;
-//	cout << "私は2018年1月20日で"<<21+1<<"歳になる"<<endl;
-//	return 0;
-//
-//}
+/*
+ * ex3.cpp
+ *
+ *  Created on: 2017/12/28
+ *      Author: PCーUSER
+ */
+
+#include "stdint.h"
+#include <iostream>
+#include <String>
+using namespace std;
+
+int main(){
+	string name;
+	uint8_t answer;
+	cout << "あなたのお名前をお伺いしてもよろしいですか？" << endl;
+	cin >> answer;
+	if(answer == 1){
+		cin >> name;
+		cout << "あなたは" << name << "さんと仰るのですね。こんにちは" << endl;
+	} else {
+		cout << "このご時世、個人情報は大事ですからね。それでは。" << endl;
+	}
+
+	return 0;
+}
+
+
